a6c5897f461f7c0e13d3e533f9e79ed11bc67b4c
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-07-27 01:55:55 +0500

    Update index.js
    M

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const prefix = ("!");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity("!help", {
-  type: "PLAYING",
-  url: "!help"
-});
+client.user.setGame('!help')
 });
 
 client.on('message', msg => {
