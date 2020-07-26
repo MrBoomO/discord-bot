93fa9009086f4e8f7521ee03252eecd16d040e65
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-07-27 01:58:01 +0500

    Update index.js
    Z

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const prefix = ("!");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame('!help')
+client.user.setGame('!help');
 });
 
 client.on('message', msg => {
