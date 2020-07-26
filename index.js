8302d0a54f566993df6f7163495234daefae819a
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-07-27 02:47:42 +0500

    Update index.js
    Q

--- index.js
+++ index.js
@@ -18,13 +18,4 @@ msg.channel.send('bot version 1.0 by MrBoom#4863');
 }
 });
 
-client.on('message', msg => {
-if (msg.content === prefix+'owner') {
-if (message.author.id === '556160607339020315') {
-msg.channel.send('YOU ARE THE BEST, MrBoom');
-} else {
-msg.channel.send('You not bot creator!');
-}
-}
-
 client.login('NjQzMDQ5Mzg4NzY3MTgyODQ5.Xcf0aQ.gMllMX1waEivtAprlNME3q5YD5c');
