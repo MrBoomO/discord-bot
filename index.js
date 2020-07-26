67c9070910d492f53110abb746d638cbfd446996
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-07-27 01:49:26 +0500

    Update index.js
    Q

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity("with depression", {
+  type: "STREAMING",
+  url: "https://www.twitch.tv/monstercat"
+});
 });
 
 client.on('message', msg => {
