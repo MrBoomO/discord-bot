dcb34859dfbdf4943a94b193439f323876759d34
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-08-06 06:01:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ const exampleEmbed = new Discord.MessageEmbed()
 	.setURL('https://mrboomoff.gamejolt.io/')
 	.setAuthor('Информация об сервере')
 	.addFields(
-		{ name: 'Название', value: 'Проверить, жив ли бот' },
-		{ name: 'Регион', value: 'Показать команды бота' },
+		{ name: 'Название', value: '${guild.name}' },
+		{ name: 'Регион', value: '${guild.region}' },
 		{ name: 'Подтверждён', value: 'Показать информацию об сервере' },
 		{ name: 'Количество пользователей', value: 'Показать информацию об пользователе' },
 	)
