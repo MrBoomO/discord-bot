8a9307bc30e5f70e295fbcf9fe8460509c6cece4
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-08-10 23:58:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ msg.channel.send(`First argument: ${args[0]}`);
 client.on('message', msg => {
 if(msg.content.startsWith(prefix+'say')) {
      msg.channel.send(msg.content.substr(4));
-     msg.delete(10);
+     msg.delete();
 }
 });
 
