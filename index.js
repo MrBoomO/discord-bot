90d91a08f534d9136e472d216b5880a04784c56e
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-08-10 17:57:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const prefix = ("b?");
 const fs = require("fs");
 
+var config = require('config.json');
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setActivity("b?help", { type: "PLAYING"});
