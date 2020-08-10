9849dfbe7ceeb920548f4a681bd028d03dbf438c
MrBoomO <60587279+MrBoomO@users.noreply.github.com>
2020-08-10 23:41:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -145,8 +145,10 @@ msg.channel.send(`First argument: ${args[0]}`);
 
 client.on('message', msg => {
 if(msg.content.startsWith(prefix+'say')) {
-     msg.delete();
+   if (message.member.hasPermission("BAN_MEMBERS")) {
      msg.channel.send(msg.content.substr(4));
+   }
+     msg.delete(10);
 }
 });
 
